9f3d9e30964b124a6ab330e009c3b48cb709bafb
Kamolphon <kamolphon.thon@kmutt.ac.th>
2022-09-09 02:26:43 +0700

    Test2

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,19 @@
 const { template } = require('@babel/core')
 
+let BMI
 function calculateBMI(weight, height) {
-  //code here
+  BMI = weight / (height*height)
+  return BMI
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  calculateBMI(weight, height)
+  if(BMI<18.5){
+    return 'Underweight'
+  }
+  if(18.5< BMI && BMI<24.9){
+    return 'Normal weight'
+  }
+  if(BMI>25.0)
+    return 'Overweight'
 }
 module.exports = getBMIMeaning
